a11b9cf7829b953c7cedb8307f91f1fbcad1647c
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-25 19:20:57 +0330

    handle register models

---
+++ app/src/main/java/ghazimoradi/soheil/recipeapp/data/models/register/RegisterStoredModel.kt
@@ -0,0 +1,3 @@
+package ghazimoradi.soheil.recipeapp.data.models.register
+
+data class RegisterStoredModel(val username: String, val hash: String)
---
+++ app/src/main/java/ghazimoradi/soheil/recipeapp/data/models/register/ResponseRegister.kt
@@ -0,0 +1,12 @@
+package ghazimoradi.soheil.recipeapp.data.models.register
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseRegister(
+    @SerializedName("hash")
+    val hash: String?, // q572587bq2405724q05
+    @SerializedName("spoonacularPassword")
+    val spoonacularPassword: String?, // meadwith31grapejam
+    @SerializedName("username")
+    val username: String? // api_123_user
+)
