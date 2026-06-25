6065749922aceb32aef6ef185b1f739c5f743976
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-25 18:35:51 +0330

    handle check network connection

---
+++ app/src/main/java/ghazimoradi/soheil/recipeapp/di/network/CheckConnection.kt
@@ -0,0 +1,27 @@
+package ghazimoradi.soheil.recipeapp.di.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import ghazimoradi.soheil.recipeapp.utils.network.CheckInternetConnection
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CheckConnection {
+
+    @Provides
+    @Singleton
+    fun provideConnectivityManager(@ApplicationContext context: Context): ConnectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    @Provides
+    @Singleton
+    fun provideCheckInternet(
+        connectivityManager: ConnectivityManager
+    ): CheckInternetConnection = CheckInternetConnection(connectivityManager)
+}
