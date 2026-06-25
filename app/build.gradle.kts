53188c1f5c64e0e6402da71baf39413e517c325e
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-25 16:51:44 +0330

    init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,101 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.androidx.navigation.safeargs)
+}
+
+android {
+    namespace = "ghazimoradi.soheil.recipeapp"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "ghazimoradi.soheil.recipeapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+
+    //KotlinMetaData
+    ksp(libs.kotlinMetadata)
+
+    //Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //fragment ktx
+    implementation(libs.androidx.fragment.ktx)
+
+    //Dagger - Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    //Room components
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    //DataStore
+    implementation(libs.androidx.datastore.preferences)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //OkHTTP client
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    //Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    //Image Loading
+    implementation(libs.coil)
+
+    //Gson
+    implementation(libs.gson)
+
+    //Calligraphy
+    implementation(libs.calligraphy3)
+    implementation(libs.viewpump)
+
+    //Other
+    implementation(libs.shimmer)
+    implementation(libs.shimmerrecyclerview)
+    implementation(libs.dynamicsizes)
+    implementation(libs.lottie)
+    implementation(libs.readmore.textview)
+}
