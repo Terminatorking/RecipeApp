ce56bf34b2447ae6ad5705c98ed8226d04253973
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-25 19:23:32 +0330

    handle ApiServices

--- app/src/main/java/ghazimoradi/soheil/recipeapp/data/network/ApiServices.kt
+++ app/src/main/java/ghazimoradi/soheil/recipeapp/data/network/ApiServices.kt
@@ -1,4 +1,42 @@
 package ghazimoradi.soheil.recipeapp.data.network
 
+import ghazimoradi.soheil.recipeapp.data.models.detail.ResponseDetail
+import ghazimoradi.soheil.recipeapp.data.models.detail.ResponseSimilar
+import ghazimoradi.soheil.recipeapp.data.models.lucky.ResponseLucky
+import ghazimoradi.soheil.recipeapp.data.models.recipe.ResponseRecipes
+import ghazimoradi.soheil.recipeapp.data.models.register.BodyRegister
+import ghazimoradi.soheil.recipeapp.data.models.register.ResponseRegister
+import ghazimoradi.soheil.recipeapp.utils.API_KEY
+import retrofit2.Response
+import retrofit2.http.*
+
 interface ApiServices {
+
+    @POST("users/connect")
+    suspend fun postRegister(
+        @Query(API_KEY) apiKey: String,
+        @Body body: BodyRegister
+    ): Response<ResponseRegister>
+
+    @GET("recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap queries: Map<String, String>
+    ): Response<ResponseRecipes>
+
+    @GET("recipes/{id}/information")
+    suspend fun getDetail(
+        @Path("id") id: Int,
+        @Query(API_KEY) apiKey: String
+    ): Response<ResponseDetail>
+
+    @GET("recipes/{id}/similar")
+    suspend fun getSimilarRecipes(
+        @Path("id") id: Int,
+        @Query(API_KEY) apiKey: String
+    ): Response<ResponseSimilar>
+
+    @GET("recipes/random")
+    suspend fun getRandomRecipe(
+        @QueryMap queries: Map<String, String>
+    ): Response<ResponseLucky>
 }
