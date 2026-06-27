89356f08b74d692e17083a08a7a81c68bcce7a8f
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-27 20:06:58 +0330

    add StepsFragment

---
+++ app/src/main/java/ghazimoradi/soheil/recipeapp/ui/fragments/steps/StepsFragment.kt
@@ -0,0 +1,9 @@
+package ghazimoradi.soheil.recipeapp.ui.fragments.steps
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class StepsFragment : Fragment() {
+
+}
