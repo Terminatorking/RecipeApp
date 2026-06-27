5fd79892ae7e7eb3aa875b10be8f3b522d663b91
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-27 22:57:14 +0330

    handle web view fragment ui

--- app/src/main/java/ghazimoradi/soheil/recipeapp/ui/fragments/webview/WebViewFragment.kt
+++ app/src/main/java/ghazimoradi/soheil/recipeapp/ui/fragments/webview/WebViewFragment.kt
@@ -1,9 +1,14 @@
 package ghazimoradi.soheil.recipeapp.ui.fragments.webview
 
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import dagger.hilt.android.AndroidEntryPoint
+import ghazimoradi.soheil.recipeapp.databinding.FragmentWebViewBinding
+import ghazimoradi.soheil.recipeapp.utils.base.BaseFragment
 
 @AndroidEntryPoint
-class WebViewFragment : Fragment() {
+class WebViewFragment : BaseFragment<FragmentWebViewBinding>() {
+
+    override val bindingInflater: (inflater: LayoutInflater) -> FragmentWebViewBinding
+        get() = FragmentWebViewBinding::inflate
 
 }
