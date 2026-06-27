b8e56a2a6fcaf28ce73fb0bb079c92b7cddfbb91
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-27 22:52:29 +0330

    handle search fragment ui

--- app/src/main/java/ghazimoradi/soheil/recipeapp/ui/fragments/search/SearchFragment.kt
+++ app/src/main/java/ghazimoradi/soheil/recipeapp/ui/fragments/search/SearchFragment.kt
@@ -1,9 +1,15 @@
 package ghazimoradi.soheil.recipeapp.ui.fragments.search
 
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import dagger.hilt.android.AndroidEntryPoint
+import ghazimoradi.soheil.recipeapp.databinding.FragmentSearchBinding
+import ghazimoradi.soheil.recipeapp.utils.base.BaseFragment
 
 @AndroidEntryPoint
-class SearchFragment : Fragment() {
+class SearchFragment : BaseFragment<FragmentSearchBinding>() {
+
+    override val bindingInflater: (inflater: LayoutInflater) -> FragmentSearchBinding
+        get() = FragmentSearchBinding::inflate
+
 
 }
